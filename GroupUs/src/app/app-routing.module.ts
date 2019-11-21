ade8ad227bb276d4f524bfa881e07eccc3ad1bd5
Steven Etgen <setgen5@gmail.com>
2019-11-20 19:38:57 -0500

    created chat page with some api calls

--- GroupUs/src/app/app-routing.module.ts
+++ GroupUs/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home', component: HomeComponent},
   {path:'sign-in',component: SignInComponent},
   {path:'sign-up',component: SignUpComponent},
-  {path:'profile', component: ProfileComponent}
+  {path:'profile', component: ProfileComponent},
+  {path:'chat', component: ChatRoomComponent}
 ];
 
 @NgModule({

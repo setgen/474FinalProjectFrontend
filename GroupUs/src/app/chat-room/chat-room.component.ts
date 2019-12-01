0eef5da14d2b0d7ea25be7bca4448b5e3e3f8a4f
Steven Etgen <setgen5@gmail.com>
2019-12-01 15:14:55 -0500

    Updadted chat html

--- GroupUs/src/app/chat-room/chat-room.component.ts
+++ GroupUs/src/app/chat-room/chat-room.component.ts
@@ -9,13 +9,22 @@ import { stringify } from 'querystring';
 })
 export class ChatRoomComponent implements OnInit {
 
+  user_id:String = "null";
+  group_id:String = "null";
+  token:string = "null"
+
   constructor() { }
 
   ngOnInit() {
     console.log("hi");
+    axios.get('localhost:5000/api/users/user_id')
+      .then((response) => {
+        this.token = response.data.token;
+        console.log(this.token);
+      });
   }
 
-  user_id:String = "null";
+
 
   //socket:any = io();
 
@@ -33,21 +42,66 @@ export class ChatRoomComponent implements OnInit {
 
   public append() {
 console.log("appended");
-    
+
+
+
+
+  // axios({
+  // method: 'get',
+  // url: 'localhost:5000/api/users/user_id',
+  // headers: {}, 
+  // data: {
+  //   username: "mtmiller",
+	//   password: "cisc474"
+  // }
+  // })
+  // .then((response) => {
+  //   this.token = response.data.token;
+  //   console.log(this.token);
+  // });
+      
+  axios.get('http://localhost:5000/api/users/group_id',
+  {
+    params:{
+      username:'mtmiller',
+      password:'cisc474'
+    }
+  })
+      .then((response) => {
+        this.group_id = response.data.group_id
+      });
+
 
     let li = document.createElement("li");
     let span = document.createElement("span");
     var messages = document.getElementById("messages");
     var message = <HTMLInputElement>document.getElementById("message");
-    messages.appendChild(li).append(message.value);
-    // messages.appendChild(li).append(data.message);
-    axios.get('http://localhost:5000/api/users/user_id')
-    .then((response) => {
-      this.user_id = response.data.user;
-    });
+    // messages.appendChild(li).append(message.value);
+    messages.appendChild(li).append(this.token);
+    axios({
+      method: 'get',
+      url: 'localhost:5000/api/users/user_id',
+      headers: {}, 
+      data: {
+        username: "mtmiller",
+        password: "cisc474"
+      }
+      })
+      .then((response) => {
+        this.token = response.data.token;
+        console.log("here");
+        console.log(this.token);
+      });
+    // axios.get('http://localhost:5000/api/users/user_id')
+    // .then((response) => {
+    //   this.user_id = response.data.user;
+    // });
     messages.appendChild(span).append("by " + this.user_id + ": " + "just now");
 
-    
+
+
+
+    console.log(this.token);
   }
 
   // axios.post('http://localhost:3000/chat/users', { userId })
